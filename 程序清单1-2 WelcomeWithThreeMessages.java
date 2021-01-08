0bbbea8fd69c638990e5c076d9e0933e55f5778a
asdxhl <71693142+asdxhl@users.noreply.github.com>
2021-01-08 18:51:31 +0800

    Update and rename WelcomeWithThreeMessages to 程序清单1-2 WelcomeWithThreeMessages.java

--- 程序清单1-2 WelcomeWithThreeMessages.java
+++ 程序清单1-2 WelcomeWithThreeMessages.java
@@ -1,7 +1,4 @@
-package demo;
-
 public class WelcomeWithThreeMessages {
-
 	public static void main(String[] args) {
 		System.out.println("Programming is fun!");
 		System.out.println("Fundamentals First");
